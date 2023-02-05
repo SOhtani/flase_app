48440e8628d65e0ac12b547b467d3d602a1c1f76
Kim Jeong-Yoo <jykim@KimnoMacBook-Air.local>
2023-02-05 13:05:42 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -2,16 +2,18 @@
 
 from flask import Flask, render_template
 
-# Flask をインスタンス化
+# app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
-# --- View側の設定 ---
-# ルートディレクトリにアクセスした場合の挙動
+# --- View 側の設定 ---
+# rootディレクトリにアクセスした場合の挙動
 @app.route('/')
-# def以下がアクセス後の操作
+
+# def 以下がアクセス後の操作
 def index():
     # return 'Hello World!'
-    return render_template('index.html')
+    return render_template('index.html') #追加
 
+# メイン関数
 if __name__ == '__main__':
     app.run()
